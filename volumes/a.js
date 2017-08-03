44e6005700b28ef71fd15f7543be868982511555
Rafael Leite <rafael.leite@controlid.com.br>
2017-08-03 14:58:15 -0300

    removing unused file

--- volumes/a.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-
-fs.appendFile('foo.txt', 'Append data!!!', function(error) {
-  if (error) {
-    console.log('Error:- ' + error);
-    throw error;
-  }
-  console.log("data appended!!");
-});
